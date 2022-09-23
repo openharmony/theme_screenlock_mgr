e098eb6419fcadecfe2da5229db71a4f41d574fa
LVB8189 <lvbai1@huawei.com>
2022-09-23 10:22:37 +0800

    Signed-off-by: LVB8189 <lvbai1@huawei.com>
     Changes to be committed:

--- test/unittest/screenlock_notify_test_instance.h
+++ test/unittest/screenlock_notify_test_instance.h
@@ -24,7 +24,7 @@ namespace OHOS {
 namespace ScreenLock {
 class ScreenlockNotifyTestInstance : public ScreenLockSystemAbilityStub {
 public:
-    ScreenlockNotifyTestInstance(const EventListenerTest &eventListener);
+    explicit ScreenlockNotifyTestInstance(const EventListenerTest &eventListener);
     virtual ~ScreenlockNotifyTestInstance();
     void OnCallBack(const SystemEvent &systemEvent) override;
 
