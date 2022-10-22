72f5421fa5fd3b55228abb76201571d1901d4d4d
LVB8189 <lvbai1@huawei.com>
2022-10-22 10:42:07 +0800

    Signed-off-by: LVB8189 <lvbai1@huawei.com>
     Changes to be committed:

--- test/unittest/screenlock_service_test.cpp
+++ test/unittest/screenlock_service_test.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define OHOS_DEBUG
 #include "screenlock_service_test.h"
 
 #include <cstdint>
