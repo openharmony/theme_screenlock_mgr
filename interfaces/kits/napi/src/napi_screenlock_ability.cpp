d8d95bf99d8b2e9891dc74e6e863e1f7aaaf722d
wangdongqi <wangdongqi2@huawei.com>
2023-01-10 16:08:40 +0800

    Signed-off-by: wangdongqi <wangdongqi2@huawei.com>
     Changes to be committed:

--- interfaces/kits/napi/src/napi_screenlock_ability.cpp
+++ interfaces/kits/napi/src/napi_screenlock_ability.cpp
@@ -214,7 +214,7 @@ void AsyncCallLockScreen(napi_env env, EventListener *lockListener)
     };
     NAPI_CALL_RETURN_VOID(env, napi_create_string_utf8(env, "AsyncCall", NAPI_AUTO_LENGTH, &resource));
     NAPI_CALL_RETURN_VOID(env, napi_create_async_work(env, nullptr, resource, execute, CompleteAsyncWork,
-                                   (void *)lockListener, &(lockListener->work)));
+                                   static_cast<void *>(lockListener), &(lockListener->work)));
     NAPI_CALL_RETURN_VOID(env, napi_queue_async_work(env, lockListener->work));
 }
 
@@ -289,7 +289,7 @@ void AsyncCallUnlockScreen(napi_env env, EventListener *unlockListener)
     };
     NAPI_CALL_RETURN_VOID(env, napi_create_string_utf8(env, "AsyncCall", NAPI_AUTO_LENGTH, &resource));
     NAPI_CALL_RETURN_VOID(env, napi_create_async_work(env, nullptr, resource, execute, CompleteAsyncWork,
-                                   (void *)unlockListener, &(unlockListener->work)));
+                                   static_cast<void *>(unlockListener), &(unlockListener->work)));
     NAPI_CALL_RETURN_VOID(env, napi_queue_async_work(env, unlockListener->work));
 }
 
