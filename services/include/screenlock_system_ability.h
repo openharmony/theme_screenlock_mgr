a280838042b472114b0c413a1722f181ec662fe5
lwf <liweifeng10@huawei.com>
2024-06-24 03:01:42 +0000

    update services/include/screenlock_system_ability.h.
    Signed-off-by: lwf <liweifeng10@huawei.com>

--- services/include/screenlock_system_ability.h
+++ services/include/screenlock_system_ability.h
@@ -154,7 +154,7 @@ public:
     int32_t SendScreenLockEvent(const std::string &event, int param) override;
     int32_t IsScreenLockDisabled(int userId, bool &isDisabled) override;
     int32_t SetScreenLockDisabled(bool disable, int userId) override;
-    int32_t SetScreenLockAuthState(int userId, int32_t authState, std::string &authToken) override;
+    int32_t SetScreenLockAuthState(int authState, int32_t userId, std::string &authToken) override;
     int32_t GetScreenLockAuthState(int userId, int32_t &authState) override;
     int Dump(int fd, const std::vector<std::u16string> &args) override;
     void SetScreenlocked(bool isScreenlocked);
