1dc86f4ff7210471071c20c51112c76399bc809d
lwf <liweifeng10@huawei.com>
2024-06-29 08:15:48 +0000

    update test/unittest/screenlock_preferences_test.cpp.
    Signed-off-by: lwf <liweifeng10@huawei.com>

--- test/unittest/screenlock_preferences_test.cpp
+++ test/unittest/screenlock_preferences_test.cpp
@@ -12,3 +12,178 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include <cstdint>
+#include <list>
+#include <string>
+#include <sys/time.h>
+
+#include "sclock_log.h"
+#include "screenlock_common.h"
+#include "securec.h"
+#include "preferences_util.h"
+#include "screenlock_preferences_test.h"
+
+
+namespace OHOS {
+namespace ScreenLock {
+using namespace testing::ext;
+
+void ScreenLockPreferenceTest::SetUpTestCase()
+{
+}
+
+void ScreenLockPreferenceTest::TearDownTestCase()
+{
+}
+
+void ScreenLockPreferenceTest::SetUp()
+{
+}
+
+void ScreenLockPreferenceTest::TearDown()
+{
+}
+
+/**
+* @tc.name: ScreenLockPreferenceTest001
+* @tc.desc: ScreenLockPreferenceTest String.
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author:
+*/
+HWTEST_F(ScreenLockPreferenceTest, ScreenLockPreferenceTest001, TestSize.Level0)
+{
+    SCLOCK_HILOGD("ScreenLockPreferenceTest String");
+    auto preferencesUtil = DelayedSingleton<PreferencesUtil>::GetInstance();
+    if (preferencesUtil == nullptr) {
+        SCLOCK_HILOGE("preferencesUtil is nullptr!");
+        return;
+    }
+    int userId = 0;
+    std::string defaulVal = "test";
+    int result = preferencesUtil->SaveString(std::to_string(userId), defaulVal);
+    std::string val = preferencesUtil->ObtainString(std::to_string(userId), defaulVal);
+    SCLOCK_HILOGD("String.[result]:%{public}d, [val]:%{public}s", result, val.c_str());
+    preferencesUtil->RemoveKey(std::to_string(userId));
+    preferencesUtil->Refresh();
+}
+
+/**
+* @tc.name: ScreenLockPreferenceTest001
+* @tc.desc: ScreenLockPreferenceTest Int.
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author:
+*/
+HWTEST_F(ScreenLockPreferenceTest, ScreenLockPreferenceTest002, TestSize.Level0)
+{
+    SCLOCK_HILOGD("ScreenLockPreferenceTest Int");
+    auto preferencesUtil = DelayedSingleton<PreferencesUtil>::GetInstance();
+    if (preferencesUtil == nullptr) {
+        SCLOCK_HILOGE("preferencesUtil is nullptr!");
+        return;
+    }
+    int userId = 0;
+    int defaulVal = 0;
+    int result = preferencesUtil->SaveInt(std::to_string(userId), defaulVal);
+    int val = preferencesUtil->ObtainInt(std::to_string(userId), defaulVal);
+    SCLOCK_HILOGD("String.[result]:%{public}d, [val]:%{public}d", result, val);
+    preferencesUtil->RemoveKey(std::to_string(userId));
+    preferencesUtil->Refresh();
+}
+
+/**
+* @tc.name: ScreenLockPreferenceTest001
+* @tc.desc: ScreenLockPreferenceTest Bool.
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author:
+*/
+HWTEST_F(ScreenLockPreferenceTest, ScreenLockPreferenceTest003, TestSize.Level0)
+{
+    SCLOCK_HILOGD("ScreenLockPreferenceTest Bool");
+    auto preferencesUtil = DelayedSingleton<PreferencesUtil>::GetInstance();
+    if (preferencesUtil == nullptr) {
+        SCLOCK_HILOGE("preferencesUtil is nullptr!");
+        return;
+    }
+    int userId = 0;
+    bool defaulVal = false;
+    int result = preferencesUtil->SaveBool(std::to_string(userId), defaulVal);
+    bool val = preferencesUtil->ObtainBool(std::to_string(userId), defaulVal);
+    SCLOCK_HILOGD("String.[result]:%{public}d, [val]:%{public}d", result, val);
+    preferencesUtil->RemoveKey(std::to_string(userId));
+    preferencesUtil->Refresh();
+}
+
+/**
+* @tc.name: ScreenLockPreferenceTest001
+* @tc.desc: ScreenLockPreferenceTest Long.
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author:
+*/
+HWTEST_F(ScreenLockPreferenceTest, ScreenLockPreferenceTest004, TestSize.Level0)
+{
+    SCLOCK_HILOGD("ScreenLockPreferenceTest Long");
+    auto preferencesUtil = DelayedSingleton<PreferencesUtil>::GetInstance();
+    if (preferencesUtil == nullptr) {
+        SCLOCK_HILOGE("preferencesUtil is nullptr!");
+        return;
+    }
+    int userId = 0;
+    int64_t defaulVal = false;
+    int result = preferencesUtil->SaveLong(std::to_string(userId), defaulVal);
+    int64_t val = preferencesUtil->ObtainLong(std::to_string(userId), defaulVal);
+    SCLOCK_HILOGD("String.[result]:%{public}d, [val]:%{public}ld", result, val);
+    preferencesUtil->RemoveKey(std::to_string(userId));
+    preferencesUtil->Refresh();
+}
+
+/**
+* @tc.name: ScreenLockPreferenceTest001
+* @tc.desc: ScreenLockPreferenceTest Float.
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author:
+*/
+HWTEST_F(ScreenLockPreferenceTest, ScreenLockPreferenceTest005, TestSize.Level0)
+{
+    SCLOCK_HILOGD("ScreenLockPreferenceTest Float");
+    auto preferencesUtil = DelayedSingleton<PreferencesUtil>::GetInstance();
+    if (preferencesUtil == nullptr) {
+        SCLOCK_HILOGE("preferencesUtil is nullptr!");
+        return;
+    }
+    int userId = 0;
+    float defaulVal = 1.0;
+    int result = preferencesUtil->SaveFloat(std::to_string(userId), defaulVal);
+    float val = preferencesUtil->ObtainFloat(std::to_string(userId), defaulVal);
+    SCLOCK_HILOGD("String.[result]:%{public}d, [val]:%{public}f", result, val);
+    preferencesUtil->RemoveKey(std::to_string(userId));
+    preferencesUtil->Refresh();
+}
+
+/**
+* @tc.name: ScreenLockPreferenceTest001
+* @tc.desc: ScreenLockPreferenceTest RmvAll.
+* @tc.type: FUNC
+* @tc.require:
+* @tc.author:
+*/
+HWTEST_F(ScreenLockPreferenceTest, ScreenLockPreferenceTest006, TestSize.Level0)
+{
+    SCLOCK_HILOGD("ScreenLockPreferenceTest RmvAll");
+    auto preferencesUtil = DelayedSingleton<PreferencesUtil>::GetInstance();
+    if (preferencesUtil == nullptr) {
+        SCLOCK_HILOGE("preferencesUtil is nullptr!");
+        return;
+    }
+    preferencesUtil->RemoveAll();
+    preferencesUtil->RefreshSync();
+}
+
+
+} // namespace ScreenLock
+} // namespace OHOS
