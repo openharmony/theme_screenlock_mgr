ebe77f44bdf88a6bae67e52a8e1dca0527aa9ba2
18671941053 <zoujunjie6@huawei.com>
2024-07-20 20:28:25 +0800

    unlock bugfix
    Signed-off-by: lwf <liweifeng10@huawei.com>

--- frameworks/js/napi/src/screenlock_callback.cpp
+++ frameworks/js/napi/src/screenlock_callback.cpp
@@ -68,11 +68,21 @@ void ScreenlockCallback::UvWorkOnCallBack(uv_work_t *work, int32_t status)
         napi_get_boolean(callBackPtr->env, screenLockSuccess, &result[static_cast<int32_t>(ARG_INFO::ARG_DATA)]);
     }
 
-    napi_get_null(callBackPtr->env, &result[static_cast<int32_t>(ARG_INFO::ARG_ERROR)]);
+    if (screenLockSuccess || callBackPtr->action == Action::UNLOCKSCREEN) {
+        napi_get_null(callBackPtr->env, &result[static_cast<int32_t>(ARG_INFO::ARG_ERROR)]);
+    } else {
+        AsyncCall::GenerateBusinessError(callBackPtr->env, callBackPtr->errorInfo,
+            &result[static_cast<int32_t>(ARG_INFO::ARG_ERROR)]);
+    }
 
     if (callBackPtr->deferred) {
-        napi_resolve_deferred(callBackPtr->env, callBackPtr->deferred,
-            result[static_cast<int32_t>(ARG_INFO::ARG_DATA)]);
+        if (screenLockSuccess || callBackPtr->action == Action::UNLOCKSCREEN) {
+            napi_resolve_deferred(callBackPtr->env, callBackPtr->deferred,
+                result[static_cast<int32_t>(ARG_INFO::ARG_DATA)]);
+        } else {
+            napi_reject_deferred(callBackPtr->env, callBackPtr->deferred,
+                result[static_cast<int32_t>(ARG_INFO::ARG_ERROR)]);
+        }
     } else {
         napi_value callbackFunc = nullptr;
         napi_value callbackResult = nullptr;
