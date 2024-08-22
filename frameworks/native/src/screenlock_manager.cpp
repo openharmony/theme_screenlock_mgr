2971e3dd22be76c3d9eb8741016c0e4307d71e3a
renguang1116 <renguang@huawei.com>
2024-08-22 15:50:58 +0800

    screenlock_manager开放RequestStrongAuth inner接口 Signed-off-by: renguang1116 <renguang@huawei.com>

--- frameworks/native/src/screenlock_manager.cpp
+++ frameworks/native/src/screenlock_manager.cpp
@@ -126,6 +126,16 @@ int32_t ScreenLockManager::Lock(int32_t userId)
     return proxy->Lock(userId);
 }
 
+int32_t ScreenLockManager::RequestStrongAuth(int reasonFlag, int32_t userId)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        SCLOCK_HILOGE("RequestStrongAuth quit because GetProxy failed.");
+        return E_SCREENLOCK_NULLPTR;
+    }
+    return proxy->RequestStrongAuth(reasonFlag, userId);
+}
+
 sptr<ScreenLockManagerInterface> ScreenLockManager::GetScreenLockManagerProxy()
 {
     sptr<ISystemAbilityManager> systemAbilityManager =
