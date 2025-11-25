74b2bfcdf9d713adfc3abff9e3c9382bfa9a7e73
speedlz <luozheng11@h-partners.com>
2025-11-25 21:02:48 +0800

    update: 更新文件 screenlock_system_ability.h
    Signed-off-by: speedlz <luozheng11@h-partners.com>

--- services/include/screenlock_system_ability.h
+++ services/include/screenlock_system_ability.h
@@ -221,7 +221,7 @@ private:
     bool CheckSystemPermission();
     void AppendPrintOtherInfo(std::string &output);
     void printCallerPid(std::string invokeName);
-    sptr<ScreenLockSystemAbility> getScreenLockSystemAbility();
+    static sptr<ScreenLockSystemAbility> getScreenLockSystemAbility();
 
     ServiceRunningState state_;
     static std::mutex instanceLock_;
